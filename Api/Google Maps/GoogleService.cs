402998a6571502a6d6cb37887345e63c5917f2ac
vkhanieva <vkhanieva@mail.ru>
2018-03-31 12:54:39 +0300

    Connect with google api

---
+++ Api/Google Maps/GoogleService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace CoreReactRedux.Api.Google_Maps
+{
+    public class GoogleService
+    {
+        public GoogleService() {}
+
+        public async Task<ValueTuple<bool, string>> GoogleRequest()
+        {
+            using (var req = new HttpClient())
+            {
+                var res = await req.GetAsync($"https://maps.googleapis.com/maps/api/directions/json?origin=54.167687,37.589559&destination=54.171845,37.602219&key=AIzaSyB-PtpIcRabdr28ggUuqCsfE4GGjrZYWi0");
+                var resString = await res.Content.ReadAsStringAsync();
+                var resJson = JObject.Parse(resString);
+
+                var failed = resJson.TryGetValue("error_message", out JToken failureMassege);
+                if (failed)
+                {
+                    return (false, failureMassege.ToString());
+                }
+
+                var resStatus = resJson["geocoded_waypoints"];
+                var resResult = resJson["routes"][0]["legs"][0];
+
+                if (!resStatus[0]["geocoder_status"].ToString().Equals("OK") || !resStatus[1]["geocoder_status"].ToString().Equals("OK"))
+                {
+                    return (false, "geocoder status is false");
+                }
+
+                var distanse = resResult["distance"]["value"].ToString();
+
+                return (true, distanse);
+            }
+        }
+    }
+}
---
+++ Controllers/GoogleController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreReactRedux.Api.Google_Maps;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreReactRedux.Controllers
+{
+    [Route("api")]
+    public class GoogleController : Controller
+    {
+        private GoogleService _service;
+
+
+        public GoogleController()
+        {
+            _service = new GoogleService();
+        }
+
+        [HttpPost("[action]")]
+        public async void Google()
+        {
+            var result = await _service.GoogleRequest();
+            if (result.Item1)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
