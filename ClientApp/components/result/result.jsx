1b6aebb82b2c4795a8494ad6fac4cc0a2d4328c8
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2018-04-01 08:10:51 +0300

    Refactor

--- ClientApp/components/result/result.jsx
+++ ClientApp/components/result/result.jsx
@@ -2,43 +2,21 @@ import * as React from 'react';
 import { Card, CardHeader, CardBody } from 'reactstrap';
 import { connect } from 'react-redux';
 import * as actionCreators from "../main/actions";
-import MapContainer from './map-container';
 import { MapDirection } from './map-direction';
 
 
 class Result extends React.PureComponent {
   render() {
-    const { loading, resultPoints } = this.props.main;
-    let template;
-    if (loading) {
-      template = () => (<div>Загрузка машины...</div>);
-    } else if (resultPoints.length === 0) { // !!!!!!!!!!!!!!!S
-      template = () => (<div>Маршрут не построен</div>);
-    } else {
-      const points = [];
-      resultPoints.forEach((pair, index) => {
-          const lat = pair.from.split(',')[0] * 1;
-          const lng = pair.from.split(',')[1] * 1;
-          points.push({ lat, lng });
-        if (index === resultPoints.length - 1) {
-          points.push(points[0]);
-        }
-      });
-      template = () => (
-        <Card style={{ marginTop: '20px' }}>
+    const { resultPoints } = this.props.main;
+    return (
+          <Card style={{ marginTop: '20px' }}>
           <CardHeader>
             Маршрут
           </CardHeader>
           <CardBody>
-            <MapContainer points4={points}/>
+            <MapDirection pointPairs={resultPoints} />
           </CardBody>
         </Card>
-      );
-    }
-    return ( <React.Fragment>
-        {template()}
-        <MapDirection pointPairs={resultPoints} />
-      </React.Fragment>
     );
   }
 }
