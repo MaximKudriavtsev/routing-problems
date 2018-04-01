2e3da51489319553688c0ff1fe98ed1388a29fdd
noomid96 <koshak-9696@inbox.ru>
2018-04-01 06:21:46 +0300

    add loading style

--- ClientApp/components/main/main.jsx
+++ ClientApp/components/main/main.jsx
@@ -6,13 +6,24 @@ import Title from './header';
 import Grid from './grid';
 import ControlPanel from './control-panel';
 import ModalWindow from './modal-window';
+import { DivWrapper } from "./main.style";
 
 class Main extends React.PureComponent {
   render() {
-    const {  rows } = this.props.main;
+    const { loading, rows } = this.props.main;
 
     return (
       <React.Fragment>
+        {
+          loading && <DivWrapper>
+            <div className="sk-folding-cube">
+              <div className="sk-cube1 sk-cube"></div>
+              <div className="sk-cube2 sk-cube"></div>
+              <div className="sk-cube4 sk-cube"></div>
+              <div className="sk-cube3 sk-cube"></div>
+            </div>
+          </DivWrapper>
+        }
         <Card>
           <Title />
           <CardBody>
@@ -28,7 +39,8 @@ class Main extends React.PureComponent {
 
 export default connect(
   state => ({
-      main: state.main
+      main: state.main,
+
   }),
   (actionCreators)
 )(Main);
